157769663ce935e3646aabb31a548f5a79f337b6
HTN <96134288+htn-dev@users.noreply.github.com>
2022-05-09 20:58:43 -0400

    initial

--- dice.py
+++ dice.py
@@ -1,4 +1,8 @@
-# 1. Get and Validate user's input
+# dice.py
 
+# ~~~ App's main code block ~~~
+# 1. Get and Validate user's input
 num_dice_input = input("How many dice do you want to roll? [1-6] ")
+num_dice = parse_input(num_dice_input)
+
 
