1f520ece394cdd9af1b850db632f15a599a5c6b0
HTN <96134288+htn-dev@users.noreply.github.com>
2022-05-09 21:03:33 -0400

    initial

--- dice.py
+++ dice.py
@@ -1,5 +1,18 @@
 # dice.py
 
+def parse_input(input_string):
+    """Return `input_string` as an integer between 1 and 6.
+
+
+    Check if `input_string` is an integer number between 1 and 6.
+
+    If so, return an integer with the same value. Otherwise, tell
+
+    the user to enter a valid number and quit the program.
+
+    """
+
+
 # ~~~ App's main code block ~~~
 # 1. Get and Validate user's input
 num_dice_input = input("How many dice do you want to roll? [1-6] ")
