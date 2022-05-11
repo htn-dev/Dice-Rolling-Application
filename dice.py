ee403ca2c161a6e99ef306f1c35a63438632bd8b
Jaden <hieu.tr.nguyen@protonmail.com>
2022-05-10 22:24:21 -0400

    initial

--- dice.py
+++ dice.py
@@ -32,7 +32,6 @@ def roll_dice(num_dice):
         roll_results.append(roll)
     return roll_results
 
-
 # ~~~ App's main code block ~~~
 # 1. Get and Validate user's input
 num_dice_input = input("How many dice do you want to roll? [1-6] ")
