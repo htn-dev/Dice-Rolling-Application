30f8d2da94a33dbc270f00b750eca24eff07e5f9
Jaden <hieu.tr.nguyen@protonmail.com>
2022-05-10 22:28:34 -0400

    initial

--- dice.py
+++ dice.py
@@ -37,4 +37,5 @@ def roll_dice(num_dice):
 num_dice_input = input("How many dice do you want to roll? [1-6] ")
 num_dice = parse_input(num_dice_input)
 # 2. Roll the dice
+roll_results = roll_dice(num_dice)
 
